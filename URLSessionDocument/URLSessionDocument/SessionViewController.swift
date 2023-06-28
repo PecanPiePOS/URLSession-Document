f50255ac1f00d7aabd42265160d2e93ebb66a0d7
PecanPie <hidra0321@naver.com>
2023-06-28 20:41:16 +0900

    Created Sessions using all the possible cases

--- URLSessionDocument/URLSessionDocument/SessionViewController.swift
+++ URLSessionDocument/URLSessionDocument/SessionViewController.swift
@@ -9,21 +9,29 @@ import UIKit
 
 class SessionViewController: UIViewController {
 
+    private let session1 = URLSession.shared
+    private let session2 = URLSession(configuration: .default)
+    private let session3 = URLSession(configuration: .ephemeral)
+    private let sessionBackground4 = URLSession(configuration: .background(withIdentifier: "back1"))
+    private let sessionBackground5 = URLSession(configuration: .background(withIdentifier: "back2"))
+    private let session6 = URLSession(configuration: .default, delegate: nil, delegateQueue: .main)
+        // delegate: URLSessionDelegate?, delegateQueue: OperationQueue?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .lightGray
+        analyzeRequest()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func analyzeRequest() {
+        guard let url = URL(string: "www.naver.com") else { return }
+        let request = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 3)
+        
+        let defaultRequest = URLRequest(url: url)
+        print(defaultRequest.cachePolicy) // default cachePolicy 는 .useProtocolCachePolicy 다.
+        print(defaultRequest.timeoutInterval) // default timeoutInterval 는 60.0s 다.
+        
+        // cachPolicy -> .reloadIgnoringLocalAndRemoteCacheData, .reloadIgnoringLocalCacheData, .reloadRevalidatingCacheData, .returnCacheDataDontLoad, .returnCacheDataElseLoad, u.seProtocolCachePolicy
     }
-    */
 
 }
