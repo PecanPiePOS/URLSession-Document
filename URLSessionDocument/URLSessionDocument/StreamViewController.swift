c773931f101b47bf8131dc50035295cfb76a25c1
PecanPie <hidra0321@naver.com>
2023-06-29 18:56:50 +0900

    Added Stream Struct and Configuration according to Document

--- URLSessionDocument/URLSessionDocument/StreamViewController.swift
+++ URLSessionDocument/URLSessionDocument/StreamViewController.swift
@@ -9,21 +9,33 @@ import UIKit
 
 class StreamViewController: UIViewController {
 
+    struct Streams {
+        var output: OutputStream
+        var input: InputStream
+    }
+    
+    lazy var boundStreams: Streams = {
+        var inputOrNil: InputStream? = nil
+        var outputOrNil: OutputStream? = nil
+        
+        // 왜 여기서 inout 이 쓰이지?
+        Stream.getBoundStreams(withBufferSize: 4096, inputStream: &inputOrNil, outputStream: &outputOrNil)
+        
+        guard let input = inputOrNil, let output = outputOrNil else {
+            fatalError("On return of `getBoundStreams`, both `inputStream` and `outputStream` will contain non-nil streams.")
+        }
+        
+//        output.delegate = self
+        output.schedule(in: .current, forMode: .default)
+        output.open()
+        
+        return Streams(output: output, input: input)
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+
