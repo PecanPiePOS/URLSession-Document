cd4731b2a656d346c5cfb09425f1433cc21890ee
PecanPie <hidra0321@naver.com>
2023-07-01 22:04:16 +0900

    Added DownloadVC to the project

---
+++ URLSessionDocument/URLSessionDocument/DownloadTaskViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadTaskViewController.swift
+//  URLSessionDocument
+//
+//  Created by KYUBO A. SHIM on 2023/07/01.
+//
+
+import UIKit
+
+class DownloadTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
