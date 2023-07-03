c09eefcf16f7a92ec5fc30efb3d18545a8d5d78a
PecanPie <hidra0321@naver.com>
2023-07-03 23:55:51 +0900

    Added file for Storyboard

---
+++ URLSessionDocument/URLSessionDocument/IntroViewController.swift
@@ -0,0 +1,29 @@
+//
+//  IntroViewController.swift
+//  URLSessionDocument
+//
+//  Created by KYUBO A. SHIM on 2023/07/03.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
