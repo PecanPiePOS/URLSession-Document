580f28cfe78a5045e4b72f39312912754b592b40
PecanPie <hidra0321@naver.com>
2023-07-05 20:58:01 +0900

    Added VC for CollectionView Test

--- URLSessionDocument/URLSessionDocument/CollectionViewTestViewController.swift
+++ URLSessionDocument/URLSessionDocument/CollectionViewTestViewController.swift
@@ -9,21 +9,62 @@ import UIKit
 
 class CollectionViewTestViewController: UIViewController {
 
+    private lazy var puzzleCollectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: setFlowLayout())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setDelegate()
+        setUI()
+        setLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setDelegate() {
+        puzzleCollectionView.delegate = self
+        puzzleCollectionView.dataSource = self
     }
-    */
+    
+    private func setUI() {
+        view.backgroundColor = .gray
+        
+        puzzleCollectionView.do {
+            $0.register(TestCollectionViewCell.self, forCellWithReuseIdentifier: "test1")
+            $0.backgroundColor = .red
+        }
+    }
+    
+    private func setLayout() {
+        view.addSubview(puzzleCollectionView)
+        
+        puzzleCollectionView.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(20)
+            $0.top.equalToSuperview().inset(100)
+            $0.height.equalTo(240)
+        }
+    }
+}
 
+extension CollectionViewTestViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.item)
+    }
+}
+
+extension CollectionViewTestViewController {
+    private func setFlowLayout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        
+        layout.itemSize = CGSize(width: 60, height: 60)
+        layout.minimumLineSpacing = 3
+        layout.minimumInteritemSpacing = 3
+        
+        return layout
+    }
 }
