65d8ff593d443750e0835f31301b2f5c3a36b62c
PecanPie <hidra0321@naver.com>
2023-07-05 20:56:34 +0900

    Added UI File for AppJam SOPT - Will be removed later

---
+++ URLSessionDocument/URLSessionDocument/TestCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TestCollectionViewCell.swift
+//  URLSessionDocument
+//
+//  Created by KYUBO A. SHIM on 2023/07/05.
+//
+
+import UIKit
+
+class TestCollectionViewCell: UICollectionViewCell {
+    
+}
