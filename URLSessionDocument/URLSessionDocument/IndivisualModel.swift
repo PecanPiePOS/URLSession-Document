64237a883d3e57b7e5f078ed1704dc1611a09357
PecanPie <hidra0321@naver.com>
2023-07-06 22:07:55 +0900

    Added Indivisual Model DTO for Project

--- URLSessionDocument/URLSessionDocument/IndivisualModel.swift
+++ URLSessionDocument/URLSessionDocument/IndivisualModel.swift
@@ -6,3 +6,56 @@
 //
 
 import Foundation
+
+    // MARK: - IndivisualDashBoardModel
+struct IndivisualDashboardModel: Codable {
+    let status: Int
+    let success: Bool
+    let message: String
+    let data: IndivisualData
+}
+
+struct IndivisualData: Codable {
+    let myPuzzle: MyPuzzle
+    let userPuzzleBoard: [UserPuzzleBoard]
+    let isReviewDay: Bool
+}
+
+    // MARK: MyPuzzle
+struct MyPuzzle: Codable {
+    let nickname: String
+    let puzzleCount: Int
+}
+
+    // MARK: UserPuzzleBoard
+struct UserPuzzleBoard: Codable {
+    let reviewDay, reviewDate: String
+    let reviewId: Int?
+}
+
+
+    // MARK: - ActionPlanModel
+struct ActionPlanModel: Codable {
+    let status: Int
+    let success: Bool
+    let message: String
+    let data: [ActionPlan]
+}
+
+struct ActionPlan: Codable {
+    let actionPlanContent, actionPlanDate: String
+}
+
+
+    // MARK: - OnGoingProjectsModel
+struct OnGoingProjectsModel: Codable {
+    let status: Int
+    let success: Bool
+    let message: String
+    let data: [ProjectInfo]
+}
+
+struct ProjectInfo: Codable {
+    let userProjectId: Int
+    let projectName: String
+}
